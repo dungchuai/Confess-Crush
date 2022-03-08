9bad7fad04608e1b896a37075deb0dad30112b0e
dungchuai <101197238+dungchuai@users.noreply.github.com>
2022-03-08 22:26:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ const CONFIG = {
     title: '',
     desc: 'Liệu em có thể trở thành người yêu của chị được không? ',
     btnYes: 'Chị đồng ý',
-    btnNo: 'Không nha :3',
+    btnNo: '',
     question: 'Hãy ấn "A" để tiếp tục',
     btnReply: 'Gửi câu trả lời',
     reply: 'A',
-    mess: 'em tạo web này để tỏ tình nhưng mà em sẽ không nhận được câu trả lời vì em mới học lập trình à',
-    messDesc: 'chị có thể trả lời qua tin nhắn fb giúp em với ạ :<',
+    mess: 'em tạo web này để tỏ tình nhưng mà em sẽ không nhận được câu trả lời vì em mới học lập trình, khả năng của em hiện tại chỉ mới đến đây :<',
+    messDesc: 'chị có thể trả lời qua tin nhắn fb được không :<',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
