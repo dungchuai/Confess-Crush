f1e316109c025842120e7b1e8dc619e1a3e0e3af
dungchuai <101197238+dungchuai@users.noreply.github.com>
2022-05-18 23:31:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    titleWeb: "tỏ tình ",
-    introTitle: ' chị eirlys',
-    introDesc: `thật ra thì em theo dõi chị lâu rồi á mà e cũng không để ý nhiều tại em nghĩ chị ở xa.
-    dạo gần đây em mới biết là chị học cùng trường đại học nên em quyết định mở lời dù em có hơi sợ tí
-    em định nhắn tin trước nhưng thấy không tiến triển mấy nên...`,
-    btnIntro: 'à hóa ra vậy',
+    titleWeb: " tỏ tình ",
+    introTitle: 'kính gửi Chị Kiều cute cute hột me phô mai que',
+    introDesc: `
+            Trời rét là tại đông
+            yêu nàng, là tại hạ`,
+    btnIntro: 'ỏ :))',
     title: '',
     desc: 'Liệu em có thể trở thành người yêu của chị được không? ',
     btnYes: 'Chị đồng ý',
     btnNo: 'không em :3',
-    question: 'Hãy ấn "A" để tiếp tục',
-    btnReply: 'Gửi câu trả lời',
-    reply: 'A',
-    mess: 'em tạo web này để tỏ tình nhưng mà em sẽ không nhận được câu trả lời vì em mới học lập trình, khả năng của em hiện tại chỉ mới đến đây :<',
-    messDesc: 'chị có thể trả lời qua tin nhắn fb được không :<',
+    question: 'hẹn chị 7h tối nay nhé',
+    btnReply: 'bạn có 2 đáp án: " ok " hoặc " ok "',
+    reply: 'ok',
+    mess: 'đến đay hết r chị eei, nhớ đăng tin nhá :)))',
+    messDesc: 'Dũng chứ Ai',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
